28d5159434d7f23b8ab7e0c588f133fc231bc00a
rohanpawar22 <rohan22pawar@gmail.com>
2020-03-29 20:32:55 +0530

    Code to Swap variables

---
+++ operators/swap.js
@@ -0,0 +1,14 @@
+let a="apple";
+let b="ball";
+
+console.log("Before Swapping ");
+console.log("a =",a);
+console.log("b =",b);
+
+c=a;
+a=b;
+b=c;
+
+console.log("After Swapping ");
+console.log("a =",a);
+console.log("b =",b);
